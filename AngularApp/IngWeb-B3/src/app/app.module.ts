0fa546d892b5ef87a53c2dc706ee21aa3d1a2887
pablosf2 <pabloeldel99@gmail.com>
2022-11-14 20:08:22 +0100

    test_pablo

--- AngularApp/IngWeb-B3/src/app/app.module.ts
+++ AngularApp/IngWeb-B3/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TestPabloComponent } from './test-pablo/test-pablo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPabloComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularApp/IngWeb-B3/src/app/test-pablo/test-pablo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-pablo',
+  templateUrl: './test-pablo.component.html',
+  styleUrls: ['./test-pablo.component.css']
+})
+export class TestPabloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
