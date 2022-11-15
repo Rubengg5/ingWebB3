556ad41c36385a610bbf4c0157bb3dba8dfcee59
pablosf2 <pabloeldel99@gmail.com>
2022-11-15 19:13:17 +0100

    iniciando cloudinary

---
+++ AngularApp/IngWeb-B3/src/app/services/cloudinary.service.ts
@@ -0,0 +1,6 @@
+this.config(['cloudinaryProvider', function (cloudinaryProvider) {
+    cloudinaryProvider
+        .set("cloud_name", "dee6pfpam")
+        .set("secure", true)
+        .set("upload_preset", "my_preset");
+  }]);
